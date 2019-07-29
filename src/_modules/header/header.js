f512676a12c7b449140480ad9020b82c6654612d
Your Name <you@example.com>
2019-07-29 18:45:24 -0300

    Updates

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -5,19 +5,24 @@ var Header = function() {
     var header = $('.header');
     var body = $('body');
     var menuOpen = $('.header__button');
+    // var menuClose = $('.header__logo');
 
     menuOpen.on('click', function(){
         header.toggleClass('-open');
         body.toggleClass('-hideOverflow');
     });
     
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 750){      
-            header.addClass('js-scroll');
-        } else {
-            header.removeClass("js-scroll");
-        }
-      });
+    // menuClose.on('click', function(){
+    //     header.removeClass('-open');
+    //     body.removeClass('-hideOverflow');
+    // });
+    // $(window).scroll(function(){
+    //     if ($(this).scrollTop() > 750){      
+    //         header.addClass('js-scroll');
+    //     } else {
+    //         header.removeClass("js-scroll");
+    //     }
+    //   });
 };
 
 module.exports = Header;
